a383b8872ac2a18dea1f84e912e3f726292f9d6c
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-09 08:12:31 -0800

    testing fix

--- Game/data/grid.gd
+++ Game/data/grid.gd
@@ -1,7 +1,7 @@
 ## Represents a grid with its size, the size of each cell in pixels, and some helper functions to
 ## calculate and convert coordinates.
 ## It's meant to be shared between game objects that need access to those values.
-class_name Grid
+class_name GridManager
 extends Resource
 
 ## The grid's rows and columns.
