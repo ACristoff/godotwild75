d6ab2a5f59023da9834b9961040964d05c8f191a
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-09 09:05:43 -0800

    folder restruct

---
+++ Game/Characters/unit_base.gd
@@ -0,0 +1,134 @@
+## Represents a unit on the game board.
+## The board manages its position inside the game grid.
+## The unit itself holds stats and a visual representation that moves smoothly in the game world.
+@tool
+class_name Unit
+extends Path2D
+
+## Emitted when the unit reached the end of a path along which it was walking.
+signal walk_finished
+
+## Shared resource of type Grid, used to calculate map coordinates.
+@export var grid: Resource
+## Distance to which the unit can walk in cells.
+@export var move_range := 6
+## The unit's move speed when it's moving along a path.
+@export var move_speed := 600.0
+## Texture representing the unit.
+@export var max_health := 8
+
+@export var health: int:
+	set(value):
+		health = clamp(value, 0, max_health)
+
+@export var damage := 2
+
+@export var skin: Texture:
+	set(value):
+		skin = value
+		if not _sprite:
+			# This will resume execution after this node's _ready()
+			await ready
+		_sprite.texture = value
+## Offset to apply to the `skin` sprite in pixels.
+@export var skin_offset := Vector2.ZERO:
+	set(value):
+		skin_offset = value
+		if not _sprite:
+			await ready
+		_sprite.position = value
+
+## Coordinates of the current cell the cursor moved to.
+var cell := Vector2.ZERO:
+	set(value):
+		# When changing the cell's value, we don't want to allow coordinates outside
+		#	the grid, so we clamp them
+		cell = grid.grid_clamp(value)
+## Toggles the "selected" animation on the unit.
+var is_selected := false:
+	set(value):
+		is_selected = value
+		if is_selected:
+			_anim_player.play("selected")
+		else:
+			_anim_player.play("idle")
+
+var _is_walking := false:
+	set(value):
+		_is_walking = value
+		set_process(_is_walking)
+
+@onready var _sprite: Sprite2D = $PathFollow2D/Sprite
+@onready var _anim_player: AnimationPlayer = $AnimationPlayer
+@onready var _path_follow: PathFollow2D = $PathFollow2D
+
+#Fix this later with a proper data struct that is descriptive
+var facing_direction = [1,2,3,4]
+#direction the unit is facing
+var current_direction = 3
+
+func _ready() -> void:
+	set_process(false)
+	_path_follow.rotates = false
+
+	cell = grid.calculate_grid_coordinates(position)
+	position = grid.calculate_map_position(cell)
+
+	# We create the curve resource here because creating it in the editor prevents us from
+	# moving the unit.
+	if not Engine.is_editor_hint():
+		curve = Curve2D.new()
+	
+	print(cell)
+	#Debug Movement
+	#var points := [
+		#Vector2(2, 2),
+		#Vector2(2, 5),
+		#Vector2(8, 5),
+		#Vector2(8, 7),
+	#]
+	#walk_along(PackedVector2Array(points))
+
+
+func _process(delta: float) -> void:
+	_path_follow.progress += move_speed * delta
+
+	if _path_follow.progress_ratio >= 1.0:
+		_is_walking = false
+		# Setting this value to 0.0 causes a Zero Length Interval error
+		_path_follow.progress = 0.00001
+		position = grid.calculate_map_position(cell)
+		curve.clear_points()
+		emit_signal("walk_finished")
+
+#All the shit for dying
+func die():
+	print("unit dies")
+	#free the tile
+	#remove from turn order
+	queue_free()
+	pass
+
+#All the stuff for taking damage
+func take_damage(damage):
+	health -= damage
+	if (health == 0):
+		die()
+		pass
+
+#All the stuff for attacking
+func attack(cells, damage):
+	prints(cells, damage)
+	pass
+
+## Starts walking along the `path`.
+## `path` is an array of grid coordinates that the function converts to map coordinates.
+func walk_along(path: PackedVector2Array) -> void:
+	if path.is_empty():
+		return
+
+	curve.add_point(Vector2.ZERO)
+	for point in path:
+		curve.add_point(grid.calculate_map_position(point) - position)
+	cell = path[-1]
+	_is_walking = true
