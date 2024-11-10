a29c3a1428c8703f4eb56b567cde10c431fcc1c3
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-09 18:42:08 -0800

    offset pathing

--- Game/Data/grid.gd
+++ Game/Data/grid.gd
@@ -27,7 +27,7 @@ func calculate_map_position(grid_position: Vector2) -> Vector2:
 
 ## Returns the coordinates of the cell on the grid given a position on the map.
 func calculate_grid_coordinates(map_position: Vector2) -> Vector2:
-	prints(map_position, (map_position / cell_size).floor() + Vector2(-1, -2))
+	#prints(map_position, (map_position / cell_size).floor() + Vector2(-1, -2))
 	return (map_position / cell_size).floor() + Vector2(-1,-2)
 
 
@@ -43,5 +43,4 @@ func grid_clamp(grid_position: Vector2) -> Vector2:
 	var out := grid_position
 	out.x = clamp(out.x, 0, size.x - 1.0)
 	out.y = clamp(out.y, 0, size.y - 1.0)
-	#print(grid_position, out)
 	return out
--- Game/Data/unit_path.gd
+++ Game/Data/unit_path.gd
@@ -18,7 +18,14 @@ func initialize(walkable_cells: Array) -> void:
 func draw(cell_start: Vector2, cell_end: Vector2) -> void:
 	clear()
 	current_path = _pathfinder.calculate_point_path(cell_start, cell_end)
-	set_cells_terrain_connect(current_path, 0, 2, 0)
+	var test_offset_path: PackedVector2Array = [] 
+	for vector in current_path:
+		var new_vector = vector + Vector2(1, 2)
+		test_offset_path.append(new_vector)
+		#print(vector)
+		pass
+	print(current_path, test_offset_path)
+	set_cells_terrain_connect(test_offset_path, 0, 2, 0)
 
 
 ## Stops drawing, clearing the drawn path and the `_pathfinder`.
