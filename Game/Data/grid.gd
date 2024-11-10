ae0a60b653474ec030a1fd52e2e6dd3e7b0618ff
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-09 18:24:11 -0800

    grid offset wip

--- Game/Data/grid.gd
+++ Game/Data/grid.gd
@@ -9,8 +9,6 @@ extends Resource
 @export var size := Vector2(20, 20)
 ## The size of a cell in pixels.
 @export var cell_size := Vector2(64, 64)
-#This could be calculated as tiles but whatever for now
-var offset_to_origin = Vector2(64, 128)
 
 ## Half of ``cell_size``
 var _half_cell_size = cell_size / 2
@@ -22,30 +20,28 @@ func calculate_mirror_position(cell):
 
 ## Returns the position of a cell's center in pixels.
 func calculate_map_position(grid_position: Vector2) -> Vector2:
-	#var new_grid_position = grid_position + Vector2(-1, -2)
-	#prints(grid_position, new_grid_position)
-	return grid_position * cell_size + _half_cell_size
+	#prints((grid_position * cell_size + _half_cell_size) + Vector2(128, 64))
+	#return grid_position * cell_size + _half_cell_size
+	return (grid_position * cell_size + _half_cell_size) + Vector2(64, 128)
 
 
 ## Returns the coordinates of the cell on the grid given a position on the map.
 func calculate_grid_coordinates(map_position: Vector2) -> Vector2:
-	#var new_map_position = map_position + offset_to_origin
-	prints('map pos', map_position, (map_position / cell_size).floor())
-	return (map_position / cell_size).floor()
+	prints(map_position, (map_position / cell_size).floor() + Vector2(-1, -2))
+	return (map_position / cell_size).floor() + Vector2(-1,-2)
 
 
 ## Returns true if the `cell_coordinates` are within the grid.
 func is_within_bounds(cell_coordinates: Vector2) -> bool:
 	var out := cell_coordinates.x >= 0 and cell_coordinates.x < size.x
+	#print(out and cell_coordinates.y >= 0 and cell_coordinates.y < size.y)
 	return out and cell_coordinates.y >= 0 and cell_coordinates.y < size.y
 
 
 ## Makes the `grid_position` fit within the grid's bounds.
 func grid_clamp(grid_position: Vector2) -> Vector2:
 	var out := grid_position
-	#out.x = clamp(out.x, 0, size.x - 1.0)
-	#out.y = clamp(out.y, 0, size.y - 1.0)
-	out.x = clamp(out.x, 1, size.x)
-	out.y = clamp(out.y, 2, size.y + 1)
-	print(out)
+	out.x = clamp(out.x, 0, size.x - 1.0)
+	out.y = clamp(out.y, 0, size.y - 1.0)
+	#print(grid_position, out)
 	return out
