c814d4b15cdb4a62362b9febc96b46bd7a7b5891
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-11 11:45:50 -0800

    audio manager

---
+++ Game/Data/audio_manager.gd
@@ -0,0 +1,28 @@
+extends AudioStreamPlayer2D
+
+
+#const levelMusic = preload("res://assets/audio/psycho-logy.mp3")
+@export var LevelMusic : Array[AudioStreamWAV]
+
+func _play_music(music: AudioStream, volume = 0.0):
+	if stream == music:
+		return
+	stream = music
+	volume_db = volume
+	play()
+	pass
+
+func play_music_level(level):
+	_play_music(LevelMusic[level], -12)
+	pass
+
+func play_sfx(stream: AudioStream, volume = 0.0):
+	var fx_player = AudioStreamPlayer2D.new()
+	fx_player.stream = stream
+	fx_player.name = "FX_Player"
+	fx_player.volume_db = volume
+	add_child(fx_player)
+	fx_player.play()
+	
+	await fx_player.finished
+	fx_player.queue_free()
