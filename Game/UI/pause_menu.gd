0b1c54d6cfc50c19efbbd4b3db8e3cc3547c5949
nicokumike <nicokumike@gmail.com>
2024-11-12 00:09:09 -0300

    merge fuck 3

--- Game/UI/pause_menu.gd
+++ Game/UI/pause_menu.gd
@@ -6,6 +6,5 @@ func _on_resume_pressed():
 	emit_signal("resume")
 	pass
 
-
 func _on_exit_pressed():
 	get_tree().quit()
--- Game/game_manager.gd
+++ Game/game_manager.gd
@@ -9,7 +9,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
-		
+
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
