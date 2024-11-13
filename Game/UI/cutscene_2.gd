d1a9a4ff8fa92b0630653da4a46282750dcfebf4
kinteyoi <calebazlin5@gmail.com>
2024-11-13 00:08:06 -0600

    Intro DONE

---
+++ Game/UI/cutscene_2.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+func GutPunch():
+	$AudioStreamPlayer2D.play()
+func Sparkle():
+	$AudioStreamPlayer2D2.play()
+func Transition():
+	$Camera2D/Transition/AnimationPlayer2.play("Trans_Out")
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_cutscene_final_animation_finished(anim_name: StringName) -> void:
+	if anim_name == ("Tengu"):
+		LevelManager.nextLevel()
