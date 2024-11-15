4a498c9fb2f60331be3022c6b817837e7efe818b
Alex Cristoffanini <acristoffanini@gmail.com>
2024-11-15 14:35:33 -0800

    rendering multiple attacks offset

--- Game/UI/attack_select.gd
+++ Game/UI/attack_select.gd
@@ -4,12 +4,18 @@ extends Node2D
 
 signal attack_chosen
 
+var vertical_offset = 40
+
 func new_button(attack_text, attack_key ):
+	var attack_count = get_children().size()
 	var new_button_inst = attack_button.instantiate()
 	self.add_child(new_button_inst)
 	new_button_inst.text = attack_text
 	new_button_inst.attack_key = attack_key
 	new_button_inst.connect("pressed", emit_chosen.bind(attack_key))
+	
+	#print(attack_count, attack_count * vertical_offset)
+	new_button_inst.position.y = attack_count * vertical_offset
 	pass
 
 func emit_chosen(attack_key):
