97e8a38fe75dc5b969dfa6a85879fccd4b3d079c
kinteyoi <calebazlin5@gmail.com>
2024-11-17 09:56:34 -0600

    levels ordering

--- Game/UI/win_screen.gd
+++ Game/UI/win_screen.gd
@@ -9,7 +9,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_onward_pressed() -> void:
-	LevelManager.switchScene(LevelManager.previous_level_index + 1)
+	LevelManager.switchScene(LevelManager.getLevelIndex("weapon_shop"))
 
 func _on_retry_pressed() -> void:
 	LevelManager.switchScene(LevelManager.previous_level_index)
